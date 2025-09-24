c0ca268594907853f6a29b14cd2f098ec62aaad9
Nisith Moragoda <nisith.nimdinu@gmail.com>
2025-09-24 10:10:32 +0530

    pinecone

---
+++ src/app/lib/pinecone.ts
@@ -0,0 +1,10 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { config } from "dotenv";
+import path from "path";
+config({ path: path.resolve(__dirname, "../../../.env.local") }); // Load environment variables from .env.local
+
+export const getPineconeClient = () => {
+    return new Pinecone({
+        apiKey: process.env.PINECONE_APIKEY!,
+    });
+};
